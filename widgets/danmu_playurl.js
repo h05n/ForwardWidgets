c6be00705828391c545ca345671d522274ac44ef
h05n <gh15851063300@icloud.com>
2025-08-26 09:00:56 +0800

    更新 danmu_playurl.js

--- widgets/danmu_playurl.js
+++ widgets/danmu_playurl.js
@@ -1273,6 +1273,7 @@ async function getCommentsById(params) {
                         "Content-Type": "application/json",
                         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                     },
+                    timeout: 15,
                 }
             );
 
